31bfeefd8392365eccef8ed47abbb34606cd2bc0
THIEUXK1 <163542844+THIEUXK1@users.noreply.github.com>
2026-02-24 17:02:30 +0700

    thêm 1 moder và sửa mục đích của Mail

---
+++ E-Form-Best/Models/ITForm/BinhLuanFormIt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Form_Best.Models.ITForm;
+
+[Table("BinhLuanFormIT")]
+public partial class BinhLuanFormIt
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("idForm")]
+    public int? IdForm { get; set; }
+
+    public string? NoiDung { get; set; }
+
+    [Column("idNguoiBinhLuan")]
+    [StringLength(50)]
+    public string? IdNguoiBinhLuan { get; set; }
+
+    [StringLength(255)]
+    public string? TenNguoiBinhLuan { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? ThoiGian { get; set; }
+
+    [StringLength(50)]
+    public string? TrangThai { get; set; }
+
+    public string? FileDinhKem { get; set; }
+}
