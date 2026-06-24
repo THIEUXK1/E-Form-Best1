49aee81622cd3c2f7c2727fc477b4875fb140286
THIEUXK1 <163542844+THIEUXK1@users.noreply.github.com>
2026-06-24 11:47:56 +0700

    ok thêm bảng  TSCN

---
+++ E-Form-Best/Models/ITForm/TscnChiTietManHinh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Form_Best.Models.ITForm;
+
+[Table("TSCN_ChiTietManHinh")]
+public partial class TscnChiTietManHinh
+{
+    [Key]
+    public int IdManHinh { get; set; }
+
+    public int? IdMay { get; set; }
+
+    [StringLength(500)]
+    public string ThongTinManHinh { get; set; } = null!;
+
+    [ForeignKey("IdMay")]
+    [InverseProperty("TscnChiTietManHinhs")]
+    public virtual TscnThongTinMay? IdMayNavigation { get; set; }
+}
---
+++ E-Form-Best/Models/ITForm/TscnChiTietOcung.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Form_Best.Models.ITForm;
+
+[Table("TSCN_ChiTietOCung")]
+public partial class TscnChiTietOcung
+{
+    [Key]
+    [Column("IdOCung")]
+    public int IdOcung { get; set; }
+
+    public int? IdMay { get; set; }
+
+    [Column("ThongTinOCung")]
+    [StringLength(500)]
+    public string ThongTinOcung { get; set; } = null!;
+
+    [ForeignKey("IdMay")]
+    [InverseProperty("TscnChiTietOcungs")]
+    public virtual TscnThongTinMay? IdMayNavigation { get; set; }
+}
